b09d3138d1ee8c42d9964c4b69d893e4970d366d
YaoJiayi <120040070@link.cuhk.edu.cn>
2024-07-15 11:36:29 -0500

    add multi-gpu support

--- precompute_tp.py
+++ precompute_tp.py
@@ -5,7 +5,7 @@ import pdb
 import argparse
 import yaml
 import os
-
+import ray
 #python precompute.py --lmcache-config-file example.yaml --data-path data
 
 parser = argparse.ArgumentParser()
@@ -45,13 +45,25 @@ doc_chunk_ids = [tokenizer.encode(doc)[1:] for doc in doc_prompts]
 
 # Create a sampling params object.
 sampling_params = SamplingParams(temperature=0, max_tokens=1)
+'''
+torch.distributed.barrier()
+torch.cuda.synchronize()
+if torch.distributed.get_rank()==0:
+    import pdb
+    pdb.set_trace()
+torch.distributed.barrier()
+torch.cuda.synchronize()
+'''
 
+print((llm.llm_engine).__dir__())
+print((llm.llm_engine.model_executor.workers[0]).__dir__())
+#print((llm.llm_engine.model_executor.workers[0]).worker.__dir__())
+#cache_driver_d = llm.llm_engine.model_executor.driver_worker.worker.model_runner.lmcache_driver
+#cache_driver_w = llm.llm_engine.model_executor._run_workers()
+executor = llm.llm_engine.model_executor
 
-cache_driver = llm.llm_engine.model_executor.driver_worker.model_runner.lmcache_driver
+executor._run_workers("set_collect_check", False, False)
 
-cache_fuse_metadata = llm.llm_engine.model_executor.driver_worker.model_runner.model.model.cache_fuse_metadata
-cache_fuse_metadata['collect'] = False
-cache_fuse_metadata['check'] = False
 
 s_start_full = [1,733, 16289, 28793, 28705]
 s_start_len = len(s_start_full) + 1
@@ -66,9 +78,8 @@ old_kvs = []
 doc_chunk_ids = [s_start+chunk_ids for chunk_ids in doc_chunk_ids]
 doc_chunk_ids = [s_start_full] + doc_chunk_ids
 
+executor._run_workers("set_collect_check", True, False)
 
-cache_fuse_metadata['collect'] = True
-cache_fuse_metadata["check"] = False
 
 input_ids = []
 doc_chunk_ids_store = []
@@ -88,30 +99,12 @@ for i in range(len(doc_chunk_ids)):
         prompts = [tokenizer.decode(doc_chunk_ids[i])]
     llm.generate(prompts, sampling_params)
     
-    k_tensor = None
-    v_tensor = None
+    executor._run_workers("concat_hack_kvs",
+                          i,
+                          doc_chunk_ids[i], doc_chunk_ids_store[i],
+                          s_start_1_len, s_start_len)    
     
-    llm_layers = llm.llm_engine.model_executor.driver_worker.model_runner.model.model.layers
-    for j in range(len(llm_layers)):
-        past_key_values = llm_layers[j].self_attn.hack_kv
-        if i == 0:
-            temp_k = past_key_values[0][:s_start_len].clone() # do not chage with s_start_1
-            temp_v = past_key_values[1][:s_start_len].clone()
-        else:
-            temp_k = past_key_values[0][s_start_1_len:len(doc_chunk_ids[i])+1].clone()
-            temp_v = past_key_values[1][s_start_1_len:len(doc_chunk_ids[i])+1].clone()    
-
-        if j == 0:
-            k_tensor = torch.unsqueeze(temp_k, dim=0)
-            v_tensor = torch.unsqueeze(temp_v, dim=0)
-        else:
-            k_tensor = torch.cat((k_tensor,torch.unsqueeze(temp_k, dim=0)), dim=0)
-            v_tensor = torch.cat((v_tensor,torch.unsqueeze(temp_v, dim=0)), dim=0)
-    k_tensor = torch.unsqueeze(k_tensor, dim=0)
-    v_tensor = torch.unsqueeze(v_tensor, dim=0)
-    kv_tensor = torch.cat([k_tensor, v_tensor], dim=0)
-    cache_driver.collect_kv_and_store(torch.tensor(doc_chunk_ids_store[i]),
-                                      kv_tensor.cpu())
+executor._run_workers("dump", cache_path) 
 #cache_driver.dump(cache_path)
 
 
