dfa558e1e82ff53dde923f9cf72d8a2074db67b8
ApostaC <yihua98@uchicago.edu>
2024-08-21 18:01:27 +0000

    Adding scripts for demo2

--- demo2-multi-node-sharing/frontend.py
+++ demo2-multi-node-sharing/frontend.py
@@ -8,7 +8,7 @@ from typing import List, Dict
 from transformers import AutoTokenizer
 
 # Change the following variables as needed
-MODEL_NAME = "mistralai/Mistral-7B-Instruct-v0.2" 
+MODEL_NAME = "lmsys/longchat-7b-16k"
 PORT1 = 8000
 PORT2 = 8001
 
@@ -23,12 +23,29 @@ tokenizer = get_tokenizer()
 
 
 @st.cache_data
-def read_context() -> str:
-    with open("context.txt", "r") as fin:
-        context = fin.read()
-    return context
+def read_chunks(file_folder) -> Dict[str, str]:
+    """
+    Read all the txt files in the folder and return the filenames
+    """
+    filenames = os.listdir(file_folder)
+    ret = {}
+    for filename in filenames:
+        if not filename.endswith("txt"):
+            continue
+        key = filename.removesuffix(".txt")
+        with open(os.path.join(file_folder, filename), "r") as fin:
+            value = fin.read()
+        ret[key] = value
 
-context = read_context()
+    return ret
+
+chunks = read_chunks("data/")
+selected_chunks = st.multiselect(
+    "Select the chunks into the context",
+    list(chunks.keys()),
+    default = [],
+    placeholder = "Select in the drop-down menu")
+contexts = [chunks[key] for key in selected_chunks]
 
 container = st.container(border=True)
 
@@ -42,19 +59,21 @@ with st.sidebar:
     session = chat_session.ChatSession(PORT1)
     session2 = chat_session.ChatSession(PORT2)
 
-    session.set_context([system_prompt] + [context])
-    session2.set_context([system_prompt] + [context])
+    session.set_context([system_prompt] + contexts)
+    session2.set_context([system_prompt] + contexts)
 
     num_tokens = tokenizer.encode(session.get_context())
     container.header(f"The context given to LLM: ({len(num_tokens)} tokens)", divider = "grey")
     container.text(session.get_context())
 
-    messages = st.container(height=400)
-    if prompt := st.chat_input("Type the question here"):
+    messages = st.container(height=300)
+    messages.markdown("*vLLM instance 1*")
+    if prompt := st.chat_input("Type the question here", key=1):
         messages.chat_message("user").write(prompt)
         messages.chat_message("assistant").write_stream(session.chat(prompt))
 
-    messages2 = st.container(height=400)
-    if prompt2 := st.chat_input("Type the question here"):
+    messages2 = st.container(height=300)
+    messages2.markdown("*vLLM instance 2*")
+    if prompt2 := st.chat_input("Type the question here", key=2):
         messages2.chat_message("user").write(prompt2)
         messages2.chat_message("assistant").write_stream(session2.chat(prompt2))
