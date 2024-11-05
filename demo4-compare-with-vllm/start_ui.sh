31e27db16d3e6b8195108291e730b28c4a077014
ApostaC <yihua98@uchicago.edu>
2024-11-05 18:13:23 +0000

    adding ui scripts

---
+++ demo4-compare-with-vllm/start_ui.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+timeout 300 bash -c 'until curl -X POST localhost:8000/v1/completions > /dev/null 2>&1;
+    do
+    echo "waiting for server to start..."
+    sleep 1
+    done'
+
+python3 preprocess.py
+streamlit run frontend-double.py
